e34a15b1c5ec335e533b8fbb993dd7cac278fdae
Amy Phung <phung.amy85@gmail.com>
2019-02-22 12:03:58 -0800

    uncommented duck_noise method

---
+++ 00-hello-world/git-pull-activity/pull_practice.rb
@@ -0,0 +1,38 @@
+# Welcome to the git pull activity! This file contains a bunch of
+# steps to demonstrate the process of working together on a piece
+# of code via git. In the interest of time we'll be uncommenting
+# code rather than writing it, but the same principals will apply.
+
+# Task 1: Pulling in changes
+# One partner should uncomment this method, add and
+# commit the changes and push to GitHub. The other
+# partner should run git pull to retrieve the changes.
+def duck_noise
+  puts "quack"
+end
+
+# Task 2: Same as task one, but switch roles
+# def truck_noise
+#   puts "honk"
+# end
+
+# Taks 3: Merging Changes
+# One partner should uncomment one method, and the other
+# partner should uncomment the other. Git will merge the two
+# sets of changes together.
+
+# def robot_noise
+#   puts "beep boop"
+# end
+
+# def train_noise
+#   puts "choo choo"
+# end
+
+# Task 4: Merge Conflict
+# Both partners should modify the same line in different ways.
+# Git doesn't know what to do in this case, so you'll have to
+# merge the two versions by hand.
+def clock_noise
+  puts ""
+end
