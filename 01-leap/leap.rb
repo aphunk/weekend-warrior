a7a62651d520c755ed3ecfc42bb74f1f4892ff56
Amy Phung <phung.amy85@gmail.com>
2019-02-23 09:43:37 -0800

    leap year passed all tests

---
+++ 01-leap/leap.rb
@@ -0,0 +1,17 @@
+# on every year that is evenly divisible by 4
+#   except every year that is evenly divisible by 100
+#     unless the year is also evenly divisible by 400
+
+# year = 2012
+
+def leap_year?(year)
+  if year % 4 == 0
+    return true
+  elsif year % 100 != 0 && year % 400 == 0
+    return true
+  else
+    return false
+  end
+end
+
+leap_year?(1996)
